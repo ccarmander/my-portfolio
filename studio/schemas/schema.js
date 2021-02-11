a358fdef0646580c63eca9437a85af4bf8f26cc9
Chris Carman <ccarmander@gmail.com>
2021-02-11 11:52:14 -0500

    removed categories

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -21,7 +21,6 @@ export default createSchema({
     // in the studio.
     post,
     author,
-    category,
     project,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
